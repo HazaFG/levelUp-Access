81aa8dc9911b9e0d122d5f95f0b40b3957936ede
HazaFG <hazaelfg@gmail.com>
2023-11-11 18:15:52 -0700

    Iniciar sesion y Registrar cuenta

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const hamburguesa = document.querySelector('.hamburguesa');
 const enlaces = document.querySelector('.clases-menu');
 const iconos = document.querySelector('.iconos-menu');
 const cuentas = document.querySelector('.cuenta-menu');
+const inicio_sesion = document.querySelector('.container-form');
+
 const bienvenidaTexto = document.querySelector('.texto-container');
 const videoJuegos = document.querySelector('.juegos-container');
 
@@ -12,6 +14,8 @@ hamburguesa.addEventListener('click', ()  =>{
     enlaces.classList.toggle('activado');
     iconos.classList.toggle('activado');
     cuentas.classList.toggle('activado');
+    //inicio_sesion.classList.toggle('activado');
+
     barras.forEach(child => {child.classList.toggle('animado')})
 
     bienvenidaTexto.classList.toggle('oculto');
